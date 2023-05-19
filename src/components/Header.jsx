134fd3829d3761b44ee7625b8baea3741d9f93fc
Your Name <you@example.com>
2023-05-19 08:26:33 +0300

    react project 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+      <header className="Header">
+        <img src="./images/Trollface.png" className="trollface"/>
+        <h2 className="header-title">Meme Generator</h2>
+        <h4>React Course -Project 3</h4>
+      </header>  
+    )
+}
